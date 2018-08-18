7f3f8c6849216ddeac00ed5c76809484a7509dd2
43081j <43081j@users.noreply.github.com>
2018-08-18 22:54:26 +0100

    rule: add no duplicate bindings rule

---
+++ src/rules/template-duplicate-bindings.ts
@@ -0,0 +1,72 @@
+/**
+ * @fileoverview Disallows duplicate names in template bindings
+ * @author James Garbutt <htttps://github.com/43081j>
+ */
+
+import {Rule} from 'eslint';
+import * as ESTree from 'estree';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'Disallows duplicate names in template bindings',
+      category: 'Best Practices',
+      url: 'https://github.com/43081j/eslint-plugin-lit/blob/master/docs/rules/template-duplicate-bindings.md'
+    }
+  },
+
+  create(context): Rule.RuleListener {
+    // variables should be defined here
+    const propertyPattern = /\b([\.@\?])?([\w\-]+)([=\s>])/g;
+    const tagPattern = /<[^\/][^>]*>/g; // todo: handle `foo=">"`
+
+    //----------------------------------------------------------------------
+    // Helpers
+    //----------------------------------------------------------------------
+
+    //----------------------------------------------------------------------
+    // Public
+    //----------------------------------------------------------------------
+
+    return {
+      'TaggedTemplateExpression': (node: ESTree.Node): void => {
+        if (node.type === 'TaggedTemplateExpression' &&
+            node.tag.type === 'Identifier' &&
+            node.tag.name === 'html') {
+
+          const html = node.quasi.quasis
+            .map((q) => q.value.raw)
+            .join('{{__lint__}}');
+
+          const tags = html.match(tagPattern);
+
+          if (tags) {
+            for (const tag of tags) {
+              const pattern = new RegExp(propertyPattern.source, 'g');
+              let match: RegExpExecArray|null;
+              let seen: string[] = [];
+
+              while ((match = pattern.exec(tag)) !== null) {
+                if (seen.includes(match[2])) {
+                  // todo: maybe report column of this property
+                  context.report({
+                    node: node,
+                    message: `Duplicate bindings are not allowed, "${match[2]}" was set multiple times.`
+                  });
+                } else {
+                  seen.push(match[2]);
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  }
+};
+
+export = rule;
---
+++ src/test/rules/template-duplicate-bindings_test.ts
@@ -0,0 +1,74 @@
+/**
+ * @fileoverview Disallows duplicate names in template bindings
+ * @author James Garbutt <htttps://github.com/43081j>
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import rule = require('../../rules/template-duplicate-bindings');
+import {RuleTester} from 'eslint';
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: 'module'
+  }
+});
+
+ruleTester.run('template-duplicate-bindings', rule, {
+  valid: [
+    {code: 'html`<x-foo .bar=${true}>`'},
+    {code: 'html`foo bar`'},
+    {code: 'html`<x-foo ?bar=${true} baz>`'},
+    {code: 'html`<x-foo foo=${true}><x-bar foo=${true}></x-bar></x-foo>`'},
+    {code: 'html`<x-foo @bar=${true}>`'}
+  ],
+
+  invalid: [
+    {
+      code: 'html`<x-foo bar bar>`',
+      errors: [
+        {
+          message: 'Duplicate bindings are not allowed, "bar" was set multiple times.',
+          line: 1,
+          column: 1
+        }
+      ]
+    },
+    {
+      code: 'html`<x-foo bar bar=${true}>`',
+      errors: [
+        {
+          message: 'Duplicate bindings are not allowed, "bar" was set multiple times.',
+          line: 1,
+          column: 1
+        }
+      ]
+    },
+    {
+      code: 'html`<x-foo><x-bar x=${true} y x=${true}></x-bar></x-foo>`',
+      errors: [
+        {
+          message: 'Duplicate bindings are not allowed, "x" was set multiple times.',
+          line: 1,
+          column: 1
+        }
+      ]
+    },
+    {
+      code: 'html`<x-foo ?foo=${true} @foo=${true} />`',
+      errors: [
+        {
+          message: 'Duplicate bindings are not allowed, "foo" was set multiple times.',
+          line: 1,
+          column: 1
+        }
+      ]
+    }
+  ]
+});
