42de211f10f591782b6f89278285c519c859451b
43081j <43081j@users.noreply.github.com>
2019-02-12 22:29:06 +0000

    chore: fix every typo

--- src/rules/no-duplicate-template-bindings.ts
+++ src/rules/no-duplicate-template-bindings.ts
@@ -1,6 +1,6 @@
 /**
  * @fileoverview Disallows duplicate names in template bindings
- * @author James Garbutt <htttps://github.com/43081j>
+ * @author James Garbutt <https://github.com/43081j>
  */
 
 import {Rule} from 'eslint';
