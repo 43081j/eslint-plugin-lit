dddf8298eea6e7cce46e2f7fbb8e91d2cd22133d
Fernando Pasik <fernando@pasik.com.ar>
2019-08-10 11:22:59 +0100

    refactor(config): sort recommended rules list

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -2,12 +2,12 @@ const config = {
   plugins: ['lit'],
 
   rules: {
-    'lit/no-duplicate-template-bindings': 'error',
-    'lit/no-legacy-template-syntax': 'error',
     'lit/attribute-value-entities': 'error',
     'lit/binding-positions': 'error',
-    'lit/no-property-change-update': 'error',
-    'lit/no-invalid-html': 'error'
+    'lit/no-duplicate-template-bindings': 'error',
+    'lit/no-invalid-html': 'error',
+    'lit/no-legacy-template-syntax': 'error',
+    'lit/no-property-change-update': 'error'
   }
 };
 
