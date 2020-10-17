b200826dc30aafed8a9e6d1f329cf2bc6c842198
thepassle <pascalschilp@gmail.com>
2020-10-17 14:37:56 +0200

    fix: edgecase 2

--- src/util.ts
+++ src/util.ts
@@ -100,12 +100,16 @@ export function getExpressionPlaceholder(
   quasi: ESTree.TemplateElement
 ): string {
   const i = node.quasi.quasis.indexOf(quasi);
+  const start = node.quasi.expressions && node.quasi.expressions[i] && node.quasi.expressions[i].range[0];
+  const end = node.quasi.expressions && node.quasi.expressions[i] && node.quasi.expressions[i].range[1];
+  const width = end - start;
 
   if (/=$/.test(quasi.value.raw)) {
-    return `"{{__Q:${i}__}}"`;
+    return `"{${new Array(isNaN(width) ? 0 : width).join('_')}}"`;
   }
-  return `{{__Q:${i}__}}`;
+  return `"{${new Array(isNaN(width) ? 0 : width).join('_')}}"`;
 }
+
 
 /**
  * Tests whether a string is a placeholder or not
