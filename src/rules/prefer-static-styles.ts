b8053d1af375c6503714da8b51e8508240303390
43081j <43081j@users.noreply.github.com>
2021-05-23 21:34:42 +0100

    rule: prefer-static-styles

---
+++ src/rules/prefer-static-styles.ts
@@ -0,0 +1,118 @@
+/**
+ * @fileoverview Enforces the use of static styles in elements
+ * @author James Garbutt <https://github.com/43081j>
+ */
+
+import {Rule} from 'eslint';
+import * as ESTree from 'estree';
+import {TemplateAnalyzer} from '../template-analyzer';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+// TODO (43081j): hope that one day estree supports class fields...
+// they've existed for a while, i suppose its just behind.
+// Remove this when they do!
+interface ClassProperty extends ESTree.BaseNode {
+  type: 'ClassProperty';
+  key: ESTree.Expression;
+  value: ESTree.Expression;
+  computed: boolean;
+  static: boolean;
+}
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'Enforces the use of static styles in elements',
+      category: 'Best Practices',
+      url:
+        'https://github.com/43081j/eslint-plugin-lit/blob/master/docs/rules/prefer-static-styles.md'
+    },
+    schema: [
+      {
+        enum: ['always', 'never']
+      }
+    ],
+    messages: {
+      always: 'Adopted stylesheets should be used instead of inline style tags',
+      never: 'Inline style tags should be used instead of static styles'
+    }
+  },
+
+  create(context): Rule.RuleListener {
+    const source = context.getSourceCode();
+    const prefer = context.options[0] !== 'never';
+
+    return {
+      'ClassExpression,ClassDeclaration': (node: ESTree.Class): void => {
+        if (
+          !prefer &&
+          node.superClass &&
+          node.superClass.type === 'Identifier' &&
+          node.superClass.name === 'LitElement'
+        ) {
+          for (const member of node.body.body) {
+            const asProp = (member as unknown) as ClassProperty;
+
+            if (
+              asProp.type === 'ClassProperty' &&
+              asProp.static &&
+              asProp.key.type === 'Identifier' &&
+              asProp.key.name === 'styles'
+            ) {
+              context.report({
+                node: member,
+                messageId: 'never'
+              });
+            }
+          }
+        }
+      },
+      AssignmentExpression: (node: ESTree.AssignmentExpression): void => {
+        if (
+          !prefer &&
+          node.left.type === 'MemberExpression' &&
+          node.left.property.type === 'Identifier' &&
+          node.left.property.name === 'adoptedStylesheets'
+        ) {
+          context.report({
+            node,
+            messageId: 'never'
+          });
+        }
+      },
+      TaggedTemplateExpression: (node: ESTree.Node): void => {
+        if (
+          node.type === 'TaggedTemplateExpression' &&
+          node.tag.type === 'Identifier' &&
+          node.tag.name === 'html'
+        ) {
+          const analyzer = TemplateAnalyzer.create(node);
+
+          if (prefer) {
+            analyzer.traverse({
+              enterElement(node) {
+                if (node.tagName === 'style' && node.sourceCodeLocation) {
+                  const loc = analyzer.resolveLocation(
+                    node.sourceCodeLocation.startTag,
+                    source
+                  );
+                  if (loc) {
+                    context.report({
+                      loc,
+                      messageId: 'always'
+                    });
+                  }
+                }
+              }
+            });
+          }
+        }
+      }
+    };
+  }
+};
+
+export = rule;
---
+++ src/test/rules/prefer-static-styles_test.ts
@@ -0,0 +1,110 @@
+/**
+ * @fileoverview Enforces the use of static styles in elements
+ * @author James Garbutt <https://github.com/43081j>
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import rule = require('../../rules/prefer-static-styles');
+import {RuleTester} from 'eslint';
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2015
+  }
+});
+
+const parser = require.resolve('@babel/eslint-parser');
+const parserOptions = {requireConfigFile: false};
+
+ruleTester.run('prefer-static-styles', rule, {
+  valid: [
+    {code: 'html`<div></div>`'},
+    {
+      parser,
+      parserOptions,
+      code: `class Foo extends LitElement {
+        static styles = css\`.foo {}\`;
+      }`
+    },
+    {
+      code: `class Foo extends LitElement {
+        whatever() {
+          this.shadowRoot.adoptedStylesheets = [
+            new CSSStyleSheet()
+          ];
+        }
+      }`
+    },
+    {
+      code: 'html`<style>.foo {}</style>`',
+      options: ['never']
+    },
+    {
+      code: 'html`<style type="text/css">.foo {}</style>`',
+      options: ['never']
+    }
+  ],
+
+  invalid: [
+    {
+      code: 'html`<style>.foo {}</style>`',
+      errors: [
+        {
+          messageId: 'always',
+          line: 1,
+          column: 6
+        }
+      ]
+    },
+    {
+      code: 'html`<style type="text/css">.foo {}</style>`',
+      errors: [
+        {
+          messageId: 'always',
+          line: 1,
+          column: 6
+        }
+      ]
+    },
+    {
+      parser,
+      parserOptions,
+      options: ['never'],
+      code: `class Foo extends LitElement {
+        static styles = css\`.foo {}\`;
+      }`,
+      errors: [
+        {
+          messageId: 'never',
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      options: ['never'],
+      code: `class Foo extends LitElement {
+        whatever() {
+          this.shadowRoot.adoptedStylesheets = [
+            new CSSStyleSheet()
+          ];
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'never',
+          line: 3,
+          column: 11
+        }
+      ]
+    }
+  ]
+});
