7e40fca504f9f24eb80c32e4b1a9fa7206b9ba4b
James Garbutt <43081j@users.noreply.github.com>
2023-05-02 20:46:33 +0100

    feat (lifecycle-super): add new lifecycle-super rule (#173)

---
+++ src/rules/lifecycle-super.ts
@@ -0,0 +1,148 @@
+/**
+ * @fileoverview Enforces calling `super` in lifecycle methods
+ * @author James Garbutt <https://github.com/43081j>
+ */
+
+import {Rule} from 'eslint';
+import * as ESTree from 'estree';
+
+const methodNames = ['connectedCallback', 'disconnectedCallback', 'update'];
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'Enforces calling `super` in lifecycle methods',
+      recommended: false,
+      url: 'https://github.com/43081j/eslint-plugin-lit/blob/master/docs/rules/lifecycle-super.md'
+    },
+    schema: [],
+    messages: {
+      callSuper:
+        'You must call `super.{{method}}` to avoid interrupting ' +
+        'the lit rendering lifecycle'
+    }
+  },
+
+  create(context): Rule.RuleListener {
+    // variables should be defined here
+    let inElement = false;
+    let currentMethod: string | null = null;
+    let superSeen = false;
+
+    //----------------------------------------------------------------------
+    // Helpers
+    //----------------------------------------------------------------------
+
+    /**
+     * Class entered
+     *
+     * @param {ESTree.Class} node Node entered
+     * @return {void}
+     */
+    function classEnter(node: ESTree.Class): void {
+      if (
+        !node.superClass ||
+        node.superClass.type !== 'Identifier' ||
+        node.superClass.name !== 'LitElement'
+      ) {
+        return;
+      }
+
+      inElement = true;
+    }
+
+    /**
+     * Class exited
+     *
+     * @return {void}
+     */
+    function classExit(): void {
+      inElement = false;
+    }
+
+    /**
+     * Method entered
+     *
+     * @param {ESTree.MethodDefinition} node Node entered
+     * @return {void}
+     */
+    function methodEnter(node: ESTree.MethodDefinition): void {
+      if (
+        !inElement ||
+        node.static === true ||
+        node.kind !== 'method' ||
+        node.key.type !== 'Identifier' ||
+        !methodNames.includes(node.key.name)
+      ) {
+        return;
+      }
+
+      currentMethod = node.key.name;
+    }
+
+    /**
+     * Method exited
+     *
+     * @param {ESTree.MethodDefinition} node Node entered
+     * @return {void}
+     */
+    function methodExit(node: ESTree.MethodDefinition): void {
+      if (currentMethod !== null && !superSeen) {
+        context.report({
+          node,
+          messageId: 'callSuper',
+          data: {
+            method: currentMethod
+          }
+        });
+      }
+
+      currentMethod = null;
+      superSeen = false;
+    }
+
+    /**
+     * Call expression entered
+     * @param {ESTree.CallExpression} node Node entered
+     * @return {void}
+     */
+    function callExpressionEnter(node: ESTree.CallExpression): void {
+      if (currentMethod === null) {
+        return;
+      }
+
+      if (
+        node.callee.type === 'MemberExpression' &&
+        node.callee.object.type === 'Super' &&
+        node.callee.property.type === 'Identifier' &&
+        node.callee.property.name === currentMethod
+      ) {
+        superSeen = true;
+      }
+    }
+
+    //----------------------------------------------------------------------
+    // Public
+    //----------------------------------------------------------------------
+
+    return {
+      ClassExpression: (node: ESTree.Node): void =>
+        classEnter(node as ESTree.Class),
+      ClassDeclaration: (node: ESTree.Node): void =>
+        classEnter(node as ESTree.Class),
+      'ClassExpression:exit': classExit,
+      'ClassDeclaration:exit': classExit,
+      MethodDefinition: (node: ESTree.Node): void =>
+        methodEnter(node as ESTree.MethodDefinition),
+      'MethodDefinition:exit': methodExit,
+      CallExpression: (node: ESTree.CallExpression): void =>
+        callExpressionEnter(node)
+    };
+  }
+};
+
+export = rule;
---
+++ src/test/rules/lifecycle-super_test.ts
@@ -0,0 +1,189 @@
+/**
+ * @fileoverview Enforces calling `super` in lifecycle methods
+ * @author James Garbutt <https://github.com/43081j>
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import rule = require('../../rules/lifecycle-super');
+import {RuleTester} from 'eslint';
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2015
+  }
+});
+
+ruleTester.run('lifecycle-super', rule, {
+  valid: [
+    'class Foo { }',
+    `class Foo {
+      connectedCallback() {}
+    }`,
+    'class Foo extends Bar { }',
+    `class Foo extends LitElement {
+      static connectedCallback() {
+        5;
+      }
+    }`,
+    `class Foo extends LitElement {
+      connectedCallback() {
+        super.connectedCallback();
+      }
+    }`,
+    `class Foo extends LitElement {
+      disconnectedCallback() {
+        super.disconnectedCallback();
+      }
+    }`,
+    `class Foo extends LitElement {
+      update() {
+        super.update();
+      }
+    }`,
+    `class Foo extends LitElement {
+      otherMethod() {
+        super.otherMethod();
+      }
+    }`
+  ],
+
+  invalid: [
+    {
+      code: `class Foo extends LitElement {
+        connectedCallback() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'connectedCallback'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `const foo = class extends LitElement {
+        connectedCallback() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'connectedCallback'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        update() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'update'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        disconnectedCallback() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'disconnectedCallback'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        disconnectedCallback() {
+          808;
+        }
+        connectedCallback() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'disconnectedCallback'},
+          line: 2,
+          column: 9
+        },
+        {
+          messageId: 'callSuper',
+          data: {method: 'connectedCallback'},
+          line: 5,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `const foo = class extends LitElement {
+        update() {
+          808;
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'update'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        connectedCallback() {
+          super.somethingElse();
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'connectedCallback'},
+          line: 2,
+          column: 9
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        connectedCallback() {
+          super.foo.connectedCallback();
+        }
+      }`,
+      errors: [
+        {
+          messageId: 'callSuper',
+          data: {method: 'connectedCallback'},
+          line: 2,
+          column: 9
+        }
+      ]
+    }
+  ]
+});
