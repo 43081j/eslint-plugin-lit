92c29646459430b3da27172adcc3630069c43464
Simon Voges <simon.voges@guidecom.de>
2024-05-26 20:38:14 +0200

    Chore: Fixed typo

--- src/test/rules/attribute-names_test.ts
+++ src/test/rules/attribute-names_test.ts
@@ -38,7 +38,7 @@ ruleTester.run('attribute-names', rule, {
         };
       }
     }`,
-    `class Foo extends Litelement {
+    `class Foo extends LitElement {
       static get properties() {
         return {
           camelCase: {type: String, attribute: 'lowercase'}
