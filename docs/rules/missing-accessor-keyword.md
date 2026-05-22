c01f5c70b1bf352f616b62c7a86446435428ca17
jpradelle <julienpradelle@gmail.com>
2026-05-22 16:35:20 +0200

    feat: add new rule missing-accessor-keyword

---
+++ docs/rules/missing-accessor-keyword.md
@@ -0,0 +1,67 @@
+# Enforces accessor keyword on lit decorated class properties
+
+With lit 3, if you are using standard decorators, it is required to use
+`accessor` keyword on class properties decorated with lit decorators
+`@property()`, `@state()`, `@query()`, `@queryAll()`, `@queryAssignedElements()`
+and `@queryAssignedNodes()`.
+
+See [lit 3 migration guide](https://lit.dev/docs/releases/upgrade/#standard-decorator-migration)
+for more details.
+
+## Rule Details
+
+This rule enforces to put `accessor` keyword on class properties when needed.
+It also provides autofix to add `accessor` keyword and can be used to migrate
+a project to lit 3 and standard decorators.
+
+The following patterns are considered as errors:
+
+```ts
+class Foo extends LitElement {
+  @property({ type: String })
+  fooProp = 'foo';
+
+  @state()
+  fooState;
+
+  @query('#foo')
+  fooQuery;
+
+  @queryAll('.foo')
+  fooQueryAll;
+
+  @queryAssignedElements({slot: 'list', selector: '.item'})
+  fooQueryAssignedElements;
+
+  @queryAssignedNodes({slot: 'header', flatten: true})
+  fooQueryAssignedNodes;
+}
+```
+
+The following patterns are not errors:
+
+```ts
+class Foo extends LitElement {
+  @property({ type: String })
+  accessor fooProp = 'foo';
+
+  @state()
+  accessor fooState;
+
+  @query('#foo')
+  accessor fooQuery;
+
+  @queryAll('.foo')
+  accessor fooQueryAll;
+
+  @queryAssignedElements({slot: 'list', selector: '.item'})
+  accessor fooQueryAssignedElements;
+
+  @queryAssignedNodes({slot: 'header', flatten: true})
+  accessor fooQueryAssignedNodes;
+}
+```
+
+## When Not To Use It
+
+If you don't use standard decorators or an old version.
---
+++ src/test/rules/missing-accessor-keyword_test.ts
@@ -0,0 +1,301 @@
+/**
+ * @fileoverview Enforces accessor keyword on lit decorated class properties
+ * @author Julien Pradelle <https://github.com/jpradelle>
+ */
+
+import {rule} from '../../rules/missing-accessor-keyword.js';
+import {RuleTester} from 'eslint';
+import parser from '@babel/eslint-parser';
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parserOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2015
+    }
+  }
+});
+
+const parserOptions = {
+  requireConfigFile: false,
+  babelOptions: {
+    plugins: [['@babel/plugin-proposal-decorators', {version: '2023-11'}]]
+  }
+};
+
+ruleTester.run('missing-accessor-keyword', rule, {
+  valid: [
+    'class Foo {}',
+    `class Foo {
+      static get properties() {
+        return {
+          whateverCaseYouWant: {type: String}
+        };
+      }
+    }`,
+    {
+      code: `class Foo extends LitElement {
+        @property({ type: String })
+        accessor fooProp = 'foo';
+
+        @state()
+        accessor fooState;
+
+        @query('#foo')
+        accessor fooQuery;
+
+        @queryAll('.foo')
+        accessor fooQueryAll;
+
+        @queryAssignedElements({slot: 'list', selector: '.item'})
+        accessor fooQueryAssignedElements;
+
+        @queryAssignedNodes({slot: 'header', flatten: true})
+        accessor fooQueryAssignedNodes;
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      }
+    },
+    {
+      code: `class Foo extends LitElement {
+        @property({ type: String }) accessor fooProp = 'foo';
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      }
+    },
+    {
+      code: `class Foo extends LitElement {
+        @property({ type: String })
+        accessor fooProp : string = 'foo';
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      }
+    },
+    {
+      code: `class Foo extends LitElement {
+        @whatever()
+        fooProp;
+
+        @whatever()
+        accessor fooProp2;
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      }
+    },
+    {
+      code: `class Foo extends NonLitElement {
+        @property()
+        fooProp;
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      }
+    }
+  ],
+
+  invalid: [
+    {
+      code: `class Foo extends LitElement {
+        @property({ type: String })
+        fooProp = 'foo';
+      }`,
+      output: `class Foo extends LitElement {
+        @property({ type: String })
+        accessor fooProp = 'foo';
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      },
+      errors: [
+        {
+          line: 3,
+          column: 9,
+          endLine: 3,
+          endColumn: 16,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'property'}
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        @state()
+        fooState;
+
+        @query('#foo')
+        fooQuery;
+
+        @queryAll('.foo')
+        fooQueryAll;
+
+        @queryAssignedElements({slot: 'list', selector: '.item'})
+        fooQueryAssignedElements;
+
+        @queryAssignedNodes({slot: 'header', flatten: true})
+        fooQueryAssignedNodes;
+      }`,
+      output: `class Foo extends LitElement {
+        @state()
+        accessor fooState;
+
+        @query('#foo')
+        accessor fooQuery;
+
+        @queryAll('.foo')
+        accessor fooQueryAll;
+
+        @queryAssignedElements({slot: 'list', selector: '.item'})
+        accessor fooQueryAssignedElements;
+
+        @queryAssignedNodes({slot: 'header', flatten: true})
+        accessor fooQueryAssignedNodes;
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      },
+      errors: [
+        {
+          line: 3,
+          column: 9,
+          endLine: 3,
+          endColumn: 17,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'state'}
+        },
+        {
+          line: 6,
+          column: 9,
+          endLine: 6,
+          endColumn: 17,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'query'}
+        },
+        {
+          line: 9,
+          column: 9,
+          endLine: 9,
+          endColumn: 20,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'queryAll'}
+        },
+        {
+          line: 12,
+          column: 9,
+          endLine: 12,
+          endColumn: 33,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'queryAssignedElements'}
+        },
+        {
+          line: 15,
+          column: 9,
+          endLine: 15,
+          endColumn: 30,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'queryAssignedNodes'}
+        }
+      ]
+    },
+    {
+      code: `class Foo extends LitElement {
+        @property({ type: String })
+        fooProp = 'foo';
+
+        @whatever()
+        fooOther;
+      }`,
+      output: `class Foo extends LitElement {
+        @property({ type: String })
+        accessor fooProp = 'foo';
+
+        @whatever()
+        fooOther;
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      },
+      errors: [
+        {
+          line: 3,
+          column: 9,
+          endLine: 3,
+          endColumn: 16,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'property'}
+        }
+      ]
+    },
+    {
+      code: `class Foo extends SubClass {
+        @property({ type: String })
+        fooProp = 'foo';
+      }`,
+      output: `class Foo extends SubClass {
+        @property({ type: String })
+        accessor fooProp = 'foo';
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      },
+      errors: [
+        {
+          line: 3,
+          column: 9,
+          endLine: 3,
+          endColumn: 16,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'property'}
+        }
+      ],
+      settings: {
+        lit: {
+          elementBaseClasses: ['SubClass']
+        }
+      }
+    },
+    {
+      code: `@customElement('foo-bar')
+      class FooBar extends SubClass {
+        @property({ type: String })
+        fooProp = 'foo';
+      }`,
+      output: `@customElement('foo-bar')
+      class FooBar extends SubClass {
+        @property({ type: String })
+        accessor fooProp = 'foo';
+      }`,
+      languageOptions: {
+        parser,
+        parserOptions
+      },
+      errors: [
+        {
+          line: 4,
+          column: 9,
+          endLine: 4,
+          endColumn: 16,
+          messageId: 'missingAccessorKeyword',
+          data: {decoratorName: 'property'}
+        }
+      ],
+      settings: {
+        lit: {
+          elementBaseClasses: ['SubClass']
+        }
+      }
+    }
+  ]
+});
